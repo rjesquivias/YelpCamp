fce48c4cc0dafa4b6bfaf254a46df5a28e97a5c5
RJ <esquiviasrj@gmail.com>
2020-10-14 10:58:57 -0700

    Campground Model Basics

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+require('dotenv/config');
+
+mongoose.connect(process.env.DB_CONNECTION, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
 	res.render('home');
-})
+});
+
+app.get('/makecampground', async (req, res) => {
+	const camp = new Campground({ title: 'My Backyard', description: 'cheap campging!' });
+	await camp.save();
+	res.send(camp);
+});
 
 app.listen(3000, () => {
 	console.log('Serving on port 3000');
