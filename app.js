341d461df4362fe1924c28aa4820abf362170df5
RJ <esquiviasrj@gmail.com>
2020-10-18 06:32:53 -0700

    A New EJS Tool For Layouts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const engine = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 require('dotenv/config');
@@ -20,6 +21,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
