69928218e3281e96af63645928572c4061abd9db
rjesquivias <RJ@lainies-mbp.lan>
2020-10-28 14:13:44 -0700

    Client-Side Form Validations & Basic Error Handler

--- app.js
+++ app.js
@@ -41,10 +41,14 @@ app.get('/campgrounds/new', (req, res) => {
 	res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', async (req, res, next) => {
+	try {
 	const campground = new Campground(req.body.campground);
 	await campground.save();
 	res.redirect(`/campgrounds/${campground._id}`);
+	} catch(e){
+		next(e);
+	}
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -68,6 +72,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
 });
+
+app.use((err, req, res, next) => {
+	res.send("Oh boy, something went wrong.");
+})
 
 app.listen(3000, () => {
 	console.log('Serving on port 3000');
