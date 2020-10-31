248b53c3b5db0136317e321b21b174a575ad1c89
RJ <esquiviasrj@gmail.com>
2020-10-30 22:27:14 -0700

    Displaying Reviews

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
 }));
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
+	const campground = await Campground.findById(req.params.id).populate('reviews');
+	console.log(campground);
 	res.render('campgrounds/show', { campground });
 }));
 
