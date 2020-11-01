98632610ed891a3802d5651ecdfbf2b64dd98dfc
RJ <esquiviasrj@gmail.com>
2020-11-01 15:31:15 -0800

    Serving Static Assets

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('views', join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(join(__dirname, 'public')));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
