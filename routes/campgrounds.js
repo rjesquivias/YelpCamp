9a14ac6e7309571e58341c80b66fbb5c91096ddf
RJ <esquiviasrj@gmail.com>
2020-11-03 09:22:26 -0800

    Flash Errors Partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,12 +32,20 @@ router.post('/', validateCampground, catchAsync(async (req, res) => {
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
 	res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id);
+    if(!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
 	res.render('campgrounds/edit', { campground });
 }));
 
