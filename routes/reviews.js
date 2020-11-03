840500f3dae8b840fc8401c2ff7c0e496e52c1a4
RJ <esquiviasrj@gmail.com>
2020-11-03 08:42:01 -0800

    Flash Success Partial

--- routes/reviews.js
+++ routes/reviews.js
@@ -21,14 +21,16 @@ router.post('/', validateReview, catchAsync(async(req, res) => {
 	const review = new Review(req.body.review);
 	campground.reviews.push(review);
 	await review.save();
-	await campground.save();
+    await campground.save();
+    req.flash('success', 'Created new review!');	
 	res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:reviewId', catchAsync(async(req, res) => {
 	const { id, reviewId } = req.params;
 	await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-	await Review.findByIdAndDelete(reviewId);
+    await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review!');	
 	res.redirect(`/campgrounds/${id}`);
 }));
 
